d78fcf768add75ba0da9cc15cfecb4240d443087
serapp <serap.paltun@id3.com.tr>
2019-08-23 12:13:01 +0300

    example

--- src/main/java/tr/com/thy/othello/web_3_0/MicroSiteApplication.java
+++ src/main/java/tr/com/thy/othello/web_3_0/MicroSiteApplication.java
@@ -4,12 +4,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 
+import javax.management.MXBean;
+
+
 @EnableCaching
 @SpringBootApplication
-public class MicroSiteApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(MicroSiteApplication.class, args);
+public class MicroSiteApplication
+{
+	
+	public static void main( String[] args )
+	{
+		SpringApplication.run( MicroSiteApplication.class, args );
 	}
-
+	
+	
 }
